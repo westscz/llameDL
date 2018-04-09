de80af8425c15cff16f015d9b94bcf30511f89e4
westscz <westscz@gmail.com>
2018-04-09 21:08:17 +0200

    Added UT to test_tagger

--- tests/test_tagger.py
+++ tests/test_tagger.py
@@ -10,6 +10,7 @@ from llamedl.tagger import Tagger
 class TestTagger(unittest.TestCase):
     def setUp(self):
         self.t = Tagger()
+        self.tags_list = ['foo','bar', 'foobar']
 
     @mock.patch("musicbrainzngs.search_artists")
     def test_get_tags_from_musicbrainzgs__match(self, search_artist_patch):
@@ -25,6 +26,34 @@ class TestTagger(unittest.TestCase):
         search_artist_patch.return_value = {'artist-count': 0}
         result = self.t.get_tags_from_musicbrainzgs('foo')
         self.assertListEqual([], result)
+
+    def test_filter_tags_with_whitelist(self):
+        self.t.whitelist = ['foo']
+        result = self.t.filter_tags_with_whitelist(self.tags_list)
+        self.assertListEqual(['foo'], result)
+
+    def test_filter_tags_with_blacklist(self):
+        self.t.blacklist = ['bar']
+        result = self.t.filter_tags_with_blacklist(self.tags_list)
+        self.assertListEqual(['foo', 'foobar'], result)
+
+    def test_filter_tags__whitelist(self):
+        self.t.whitelist = ['foo']
+        self.t.blacklist = None
+        result = self.t.filter_tags(self.tags_list)
+        self.assertListEqual(['foo'], result)
+
+    def test_filter_tags__blacklist(self):
+        self.t.whitelist = None
+        self.t.blacklist = ['bar']
+        result = self.t.filter_tags(self.tags_list)
+        self.assertListEqual(['foo', 'foobar'], result)
+
+    def test_filter_tags__all(self):
+        self.t.whitelist = ['foo', 'foobar']
+        self.t.blacklist = ['foo']
+        result = self.t.filter_tags(self.tags_list)
+        self.assertListEqual(['foobar'], result)
 
 
 if __name__ == '__main__':
